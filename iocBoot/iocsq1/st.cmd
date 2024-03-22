0fd999fdcde7beb22ba4e0fe78c1d0a4f4b0c913
Nick Marks <nmarks99@gmail.com>
2024-03-22 11:35:37 -0600

    minor changes at FAT

--- iocBoot/iocsq1/st.cmd
+++ iocBoot/iocsq1/st.cmd
@@ -1,6 +1,9 @@
 # ../../bin/${EPICS_HOST_ARCH}/sq1 st.cmd
 < envPaths
 
+# Shell prompt
+epicsEnvSet("IOCSH_PS1", "$(IOC)>")
+
 epicsEnvSet("PREFIX", "sq1:")
 epicsEnvSet("LUA_SCRIPT_PATH", "./lua_scripts")
 
@@ -8,7 +11,7 @@ dbLoadDatabase("../../dbd/iocsq1Linux.dbd")
 iocsq1Linux_registerRecordDeviceDriver(pdbbase)
 
 # Add IP address of Windows machine
-epicsEnvSet("EPICS_CA_ADDR_LIST", "164.54.60.205")
+# epicsEnvSet("EPICS_CA_ADDR_LIST", "164.54.60.205")
 
 dbLoadRecords("./sq1.db", "P=$(PREFIX)")
 
